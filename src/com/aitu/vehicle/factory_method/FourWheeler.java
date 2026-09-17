e0ce9451060077370eead084dac124c19ae4b9c8
rzxmiko <250231@astanait.edu.kz>
2026-09-17 13:06:48 +0500

    new

---
+++ src/com/aitu/vehicle/factory_method/FourWheeler.java
@@ -0,0 +1,8 @@
+package com.aitu.vehicle.factory_method;
+
+public class FourWheeler implements Vehicle{
+    @Override
+    public void assemble() {
+        System.out.println("Assembling a Four-Wheeler Vehicle (Car).");
+    }
+}
