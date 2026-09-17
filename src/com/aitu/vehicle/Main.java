0c1df16a102db9e4688b48c80a858e5c02284307
rzxmiko <250231@astanait.edu.kz>
2026-09-17 13:33:11 +0500

    part a ready

---
+++ src/com/aitu/vehicle/Main.java
@@ -0,0 +1,13 @@
+package com.aitu.vehicle;
+import com.aitu.vehicle.factory_method.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Part A: Factory Method");
+
+        VehicleFactory bikeFactory = new TwoWheelerFactory();
+        bikeFactory.deliverVehicle();
+
+        VehicleFactory carFactory = new FourWheelerFactory();
+        carFactory.deliverVehicle();
+    }
+}
---
+++ src/com/aitu/vehicle/factory_method/VehicleFactory.java
@@ -0,0 +1,11 @@
+package com.aitu.vehicle.factory_method;
+
+public abstract class VehicleFactory {
+    public abstract Vehicle createVehicle();
+
+    public void deliverVehicle(){
+        Vehicle vehicle = createVehicle();
+        vehicle.assemble();
+        System.out.println("Vehicle is ready for delivery!\n");
+    }
+}
