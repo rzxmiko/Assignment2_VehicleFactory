bbfcdfd678fcf17e2ffa197a61a6e5c91c3e1703
rzxmiko <250231@astanait.edu.kz>
2026-09-17 14:01:17 +0500

    part b and main

--- src/com/aitu/vehicle/Main.java
+++ src/com/aitu/vehicle/Main.java
@@ -1,13 +1,32 @@
 package com.aitu.vehicle;
-import com.aitu.vehicle.factory_method.*;
+import com.aitu.vehicle.abstract_factory.Engine;
+import com.aitu.vehicle.abstract_factory.SafetyFeature;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Part A: Factory Method");
 
-        VehicleFactory bikeFactory = new TwoWheelerFactory();
-        bikeFactory.deliverVehicle();
+        com.aitu.vehicle.factory_method.VehicleFactory bikeMethodFactory = new com.aitu.vehicle.factory_method.TwoWheelerFactory();
+        bikeMethodFactory.deliverVehicle();
 
-        VehicleFactory carFactory = new FourWheelerFactory();
-        carFactory.deliverVehicle();
+        com.aitu.vehicle.factory_method.VehicleFactory carMethodFactory = new com.aitu.vehicle.factory_method.FourWheelerFactory();
+        carMethodFactory.deliverVehicle();
+
+        System.out.println("Part B: Factory Method");
+
+        System.out.println("Two-Wheeler Family");
+        com.aitu.vehicle.abstract_factory.VehicleFactory bikeAbstractFactory = new com.aitu.vehicle.abstract_factory.TwoWheelerFactory();
+        Engine bikeEngine = bikeAbstractFactory.createEngine();
+        SafetyFeature bikeSafety = bikeAbstractFactory.createSafetyFeature();
+        bikeEngine.getSpecs();
+        bikeSafety.deploy();
+
+        System.out.println();
+
+        System.out.println("Four-Wheeler Family");
+        com.aitu.vehicle.abstract_factory.VehicleFactory carAbstractFactory = new com.aitu.vehicle.abstract_factory.FourWheelerFactory();
+        Engine carEngine = carAbstractFactory.createEngine();
+        SafetyFeature carSafety = carAbstractFactory.createSafetyFeature();
+        carEngine.getSpecs();
+        carSafety.deploy();
     }
 }
--- src/com/aitu/vehicle/factory_method/FourWheeler.java
+++ src/com/aitu/vehicle/factory_method/FourWheeler.java
@@ -3,6 +3,6 @@ package com.aitu.vehicle.factory_method;
 public class FourWheeler implements Vehicle{
     @Override
     public void assemble() {
-        System.out.println("Assembling a Four-Wheeler Vehicle (Car).");
+        System.out.println("Assembling a Four-Wheeler Vehicle(Car).");
     }
 }
