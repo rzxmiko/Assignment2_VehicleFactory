7c1ee10bfbcea97b1f806cfa8277cc1262f2f1a5
rzxmiko <250231@astanait.edu.kz>
2026-09-18 16:07:19 +0500

    easy

--- src/com/aitu/vehicle/Main.java
+++ src/com/aitu/vehicle/Main.java
@@ -1,32 +1,32 @@
 package com.aitu.vehicle;
+
 import com.aitu.vehicle.abstract_factory.Engine;
 import com.aitu.vehicle.abstract_factory.SafetyFeature;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Part A: Factory Method");
 
-        com.aitu.vehicle.factory_method.VehicleFactory bikeMethodFactory = new com.aitu.vehicle.factory_method.TwoWheelerFactory();
-        bikeMethodFactory.deliverVehicle();
+        runFactoryMethod(new com.aitu.vehicle.factory_method.TwoWheelerFactory());
+        runFactoryMethod(new com.aitu.vehicle.factory_method.FourWheelerFactory());
 
-        com.aitu.vehicle.factory_method.VehicleFactory carMethodFactory = new com.aitu.vehicle.factory_method.FourWheelerFactory();
-        carMethodFactory.deliverVehicle();
+        System.out.println("Part B: Abstract Factory");
 
-        System.out.println("Part B: Factory Method");
+        runAbstractFactory("Two-Wheeler Family", new com.aitu.vehicle.abstract_factory.TwoWheelerFactory());
+        runAbstractFactory("Four-Wheeler Family", new com.aitu.vehicle.abstract_factory.FourWheelerFactory());
+    }
 
-        System.out.println("Two-Wheeler Family");
-        com.aitu.vehicle.abstract_factory.VehicleFactory bikeAbstractFactory = new com.aitu.vehicle.abstract_factory.TwoWheelerFactory();
-        Engine bikeEngine = bikeAbstractFactory.createEngine();
-        SafetyFeature bikeSafety = bikeAbstractFactory.createSafetyFeature();
-        bikeEngine.getSpecs();
-        bikeSafety.deploy();
+    private static void runFactoryMethod(com.aitu.vehicle.factory_method.VehicleFactory factory) {
+        factory.deliverVehicle();
+    }
 
+    private static void runAbstractFactory(String title, com.aitu.vehicle.abstract_factory.VehicleFactory factory) {
+        System.out.println(title);
+        Engine engine = factory.createEngine();
+        SafetyFeature safety = factory.createSafetyFeature();
+
+        engine.getSpecs();
+        safety.deploy();
         System.out.println();
-
-        System.out.println("Four-Wheeler Family");
-        com.aitu.vehicle.abstract_factory.VehicleFactory carAbstractFactory = new com.aitu.vehicle.abstract_factory.FourWheelerFactory();
-        Engine carEngine = carAbstractFactory.createEngine();
-        SafetyFeature carSafety = carAbstractFactory.createSafetyFeature();
-        carEngine.getSpecs();
-        carSafety.deploy();
     }
 }
